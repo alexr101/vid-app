aba22f78217781416f33afe07e620140373b0119
alexr101 <alex_r_101@hotmail.com>
2017-09-16 13:41:40 -0600

    add basic MVC route

---
+++ Vid App/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vid_App.Models;
+
+namespace Vid_App.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Interstellar" };
+
+            return View(movie);
+        }
+    }
+}
