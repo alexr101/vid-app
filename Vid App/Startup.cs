a9c68c3e40f0984169721778d83359fae88092c1
alexr101 <alex_r_101@hotmail.com>
2017-09-17 18:20:03 -0400

    add authentication manually

---
+++ Vid App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vid_App.Startup))]
+namespace Vid_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
